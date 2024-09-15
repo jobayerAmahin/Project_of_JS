c72ff538d60578fc10b4812fd56cca7a1ae9e968
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-15 21:54:37 +0300

    Adding new JS file and HTML file for practicing DOM

---
+++ main.js
@@ -0,0 +1,11 @@
+// console.log("Hello");
+// const a=document.querySelectorAll("div");
+// for(const sec of a){
+//     sec.style.border="2px solid blue";
+//     sec.style.margin="5px";
+//     sec.style.borderRadius="5px";
+//     sec.style.backgroundColor="gray";
+// }
+
+const lists=document.getElementById("listItem");
+console.log(lists);
