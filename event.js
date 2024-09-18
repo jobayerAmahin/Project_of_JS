f9bdd4ffa168465a65c4f69e402b3e68bb693dbb
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-18 18:36:38 +0300

    Adding project 2 of text area

--- event.js
+++ event.js
@@ -14,3 +14,21 @@ function minus(){
     sum-=1;
     dig.innerText=sum;
 }
+
+//Text or Comment Get and Write
+const mainSec=document.getElementById('commentGet');
+const tArea=document.getElementById('tArea');
+const tButton=document.getElementById('tButton');
+const cancel=document.getElementById('cancel');
+
+tButton.addEventListener('click',function(){
+    const tVal=tArea.value;
+    const p=document.createElement('p');
+    p.innerText=tVal;
+    mainSec.appendChild(p);
+    p.setAttribute("class","flex justify-center")
+})
+
+cancel.addEventListener('click',function(){
+    tArea.value=''
+})
